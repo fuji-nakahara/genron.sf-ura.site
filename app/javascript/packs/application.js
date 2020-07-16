eef7998ec563c219bb45cecac63183e67cb0f227
Fuji Nakahara <fujinakahara2032@gmail.com>
2020-07-16 22:39:43 +0900

    Introduce stimulus, eslint and prettier

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,8 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import Rails from '@rails/ujs';
-import 'bootstrap/dist/js/bootstrap.bundle';
+import Rails from '@rails/ujs'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'controllers'
 
-Rails.start();
+Rails.start()
