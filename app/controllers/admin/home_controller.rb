f05dcd3dc04874b52967a893fb06d375a26a23aa
Fuji Nakahara <fujinakahara2032@gmail.com>
2020-07-18 18:23:46 +0900

    Add users table to admin

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -5,6 +5,7 @@ module Admin
     include AdminRequired
 
     def show
+      @users = User.all.reverse_order
       @genron_sf_student_ids = Student.left_joins(:user).where(users: { id: nil }).pluck(:genron_sf_id).compact
       @twitter_screen_names = User.joins(:student).merge(Student.where(genron_sf_id: nil)).pluck(:twitter_screen_name)
     end
