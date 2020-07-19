28112f9d49d3920d0b563f2ed0913fa2583025e4
Fuji Nakahara <fujinakahara2032@gmail.com>
2020-07-20 00:50:03 +0900

    Remove user list from admin

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -5,7 +5,6 @@ module Admin
     include AdminRequired
 
     def show
-      @users = User.all.reverse_order
       @links = Link.all.reverse_order.limit(10)
       @genron_sf_student_ids = Student.left_joins(:user).where(users: { id: nil }).pluck(:genron_sf_id)
       @twitter_screen_names = User.joins(:student).merge(Student.where(genron_sf_id: nil)).pluck(:twitter_screen_name)
