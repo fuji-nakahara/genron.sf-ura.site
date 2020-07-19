59befdd1c4fbb16f18fc9cd55cfeee83425b1f6a
Fuji Nakahara <fujinakahara2032@gmail.com>
2020-07-20 00:44:53 +0900

    Add new votes to home#show

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@
 class HomeController < ApplicationController
   def show
     @kadais = Kadai.newest3
+    @votes = Vote.includes(:user, work: :student).reverse_order.limit(6)
     score_table = Rails.cache.fetch("score_table/#{Kadai::LATEST_YEAR}", expires_in: 1.day) do
       GenronSF::ScoreTable.get(year: Kadai::LATEST_YEAR).to_h
     end
