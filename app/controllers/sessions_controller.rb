8304601b9fc75d76e6652f2314fd58145a2043bb
Fuji Nakahara <fujinakahara2032@gmail.com>
2020-10-02 01:27:55 +0900

    Improve student detection on sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       TwitterFollowJob.perform_later(user.twitter_id)
 
       student = StudentTwitterCandidate.find_by(twitter_screen_name: auth_hash.info.nickname)&.student
-      student ||= Student.find_by(genron_sf_id: auth_hash.info.nickname)
+      student ||= Student.find_by(genron_sf_id: auth_hash.info.nickname.sub('_', '').downcase)
       if student
         Raven.capture_message(
           '受講生がログインした可能性があります',
