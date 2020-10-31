bfcbda7d184f9df3f34c7da811d9569d4829a766
Fuji Nakahara <fujinakahara2032@gmail.com>
2020-10-31 20:37:35 +0900

    Replace gem 'genron_sf' from local to github

--- genron_sf/lib/genron_sf/error.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module GenronSF
-  class Error < StandardError; end
-  class HTTPError < Error; end
-end
--- genron_sf/lib/genron_sf/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module GenronSF
-  VERSION = '0.1.0'
-end
