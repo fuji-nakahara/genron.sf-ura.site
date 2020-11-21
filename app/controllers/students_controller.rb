55b458c94040d4d240f00d4f6fff44b786ae9454
Fuji Nakahara <fujinakahara2032@gmail.com>
2020-11-21 18:20:31 +0900

    Update students#show

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,8 @@
 class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
+    @jissakus = @student.jissakus.includes(:prize, :kadai, :voters).reverse_order
+    @kougais = @student.kougais.includes(:kadai, :voters).reverse_order
   end
 
   def find
