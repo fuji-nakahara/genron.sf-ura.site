7a90b4b4f78394c2c036c40b7184418ffeb365c9
Fuji Nakahara <fujinakahara2032@gmail.com>
2021-01-29 00:05:58 +0900

    Remove needless eager loading

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,7 @@ class HomeController < ApplicationController
   def show
     @kadais = Kadai.newest3
     @votes = Vote.includes(:user, work: :student).reverse_order.limit(10)
-    @students = Student.includes(:user)
-                       .joins(works: :kadai)
+    @students = Student.joins(works: :kadai)
                        .merge(Kadai.where(year: @kadais.last.year))
                        .with_votes_sum
                        .order(votes_sum: :desc, genron_sf_id: :asc)
