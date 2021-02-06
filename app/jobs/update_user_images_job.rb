2188d432f583a389779ca73237eb76a7f4411d59
Fuji Nakahara <fujinakahara2032@gmail.com>
2021-02-06 11:18:02 +0900

    Simplify UpdateUserImagesJob

--- app/jobs/update_user_images_job.rb
+++ app/jobs/update_user_images_job.rb
@@ -2,8 +2,6 @@
 
 class UpdateUserImagesJob < ApplicationJob
   def perform(user_relation: User.all)
-    failed_screen_names = []
-
     user_relation.find_each do |user|
       image_uri = URI.parse(user.image_url)
 
@@ -20,21 +18,11 @@ class UpdateUserImagesJob < ApplicationJob
           user.destroy!
           Sentry.capture_message('Deleted a user', level: :info, extra: user.as_json)
         end
-      when Net::HTTPServiceUnavailable
-        failed_screen_names << user.twitter_screen_name
       else
         response.value
       end
 
       sleep 0.1
-    end
-
-    unless failed_screen_names.empty?
-      Sentry.capture_message(
-        'Failed to fetch image_url',
-        level: :info,
-        extra: { twitter_screen_names: failed_screen_names },
-      )
     end
   end
 end
