8a7afe798af0210f610373516bb522d796f2d5f7
Fuji Nakahara <fujinakahara2032@gmail.com>
2021-05-03 17:34:38 +0900

    Replace score chart with react version

--- app/javascript/components/ScoreChart.tsx
+++ app/javascript/components/ScoreChart.tsx
@@ -1,9 +1,73 @@
-import { FC } from "react";
+import * as palette from "google-palette";
+import { FC, useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
+import ChartComponent from "react-chartjs-2";
 
-type Props = {};
+type Props = {
+  url: string;
+};
 
-const ScoreChart: FC<Props> = () => {
-  return <div>ScoreChart</div>;
+type ScoreTable = {
+  student: string;
+  scores: number[];
+}[];
+
+async function getScoreTable(url: string): Promise<ScoreTable> {
+  const response = await fetch(url);
+  if (response.ok) {
+    return await response.json();
+  } else {
+    throw new Error(
+      `Failed to fetch scoreTable from ${url} (${response.status})`
+    );
+  }
+}
+
+const ScoreChart: FC<Props> = ({ url }) => {
+  const [scoreTable, setScoreTable] = useState<ScoreTable | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const scoreTable = await getScoreTable(url);
+      setScoreTable(scoreTable);
+    })();
+  }, [url]);
+
+  if (scoreTable === null) {
+    return (
+      <div className="d-flex justify-content-center">
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </div>
+    );
+  }
+
+  const labelLength = Math.max(...scoreTable.map((row) => row.scores.length));
+  const labels = [...Array(labelLength).keys()].map((i) => `第${i + 1}回`);
+
+  const colors = palette("tol-rainbow", scoreTable.length);
+  const datasets = scoreTable.map((row, i) => {
+    let sum = 0;
+    const accumulatedScores = row.scores.map((score) => {
+      sum += score;
+      return sum;
+    });
+
+    return {
+      label: row.student,
+      data: accumulatedScores,
+      backgroundColor: `#${colors[i]}`,
+      borderColor: `#${colors[i]}`,
+      pointRadius: 8,
+      pointHoverRadius: 12,
+      tension: 0.4,
+    };
+  });
+
+  return (
+    <ChartComponent type="line" data={{ labels: labels, datasets: datasets }} />
+  );
 };
 
 export default ScoreChart;
--- app/javascript/packs/scores.tsx
+++ app/javascript/packs/scores.tsx
@@ -3,7 +3,10 @@ import ScoreChart from "../components/ScoreChart";
 
 document.addEventListener("DOMContentLoaded", () => {
   const scoreChart = document.getElementById("score-chart");
-  if (scoreChart) {
-    ReactDOM.render(<ScoreChart></ScoreChart>, scoreChart);
+  if (scoreChart && scoreChart.dataset.url) {
+    ReactDOM.render(
+      <ScoreChart url={scoreChart.dataset.url}></ScoreChart>,
+      scoreChart
+    );
   }
 });
