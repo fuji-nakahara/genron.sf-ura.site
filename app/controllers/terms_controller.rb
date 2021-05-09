9534d840b2a8173a409a1762a9ad5fbbdfc5bb90
Fuji Nakahara <fujinakahara2032@gmail.com>
2021-05-09 17:26:46 +0900

    Remove unused instance variable in terms#index

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -4,9 +4,5 @@ class TermsController < ApplicationController
   def index
     @kadais = Kadai.newest3
     @votes = Vote.includes(:user, work: %i[kadai student]).reverse_order.limit(10)
-    @students = Student.joins(works: :kadai)
-                       .merge(Kadai.where(year: @kadais.last.year))
-                       .with_votes_sum
-                       .order(votes_sum: :desc, genron_sf_id: :asc)
   end
 end
