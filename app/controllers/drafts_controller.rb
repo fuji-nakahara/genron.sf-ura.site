e176f93b05100fd6356e65363f060c4880a41a90
Fuji Nakahara <fujinakahara2032@gmail.com>
2022-04-11 21:33:43 +0900

    Feature: Call for Comments (#624)
    * Add drafts#new
    
    * Implement drafts#create
    
    * Add TweetDraftSubmittedJob

---
+++ app/controllers/drafts_controller.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class DraftsController < ApplicationController
+  before_action :require_current_user, only: %i[new create]
+
+  def new
+    @draft = Draft.new
+  end
+
+  def create
+    @draft = Draft.new(
+      draft_params.merge( # TODO: 課題と種類をユーザが選択できるようにする
+        kadai: Kadai.newest_order.first,
+        student: current_user.student,
+        kind: :kougai,
+      ),
+    )
+
+    if @draft.save
+      TweetDraftSubmittedJob.perform_later(@dfart)
+      redirect_to root_path, notice: '投稿しました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def draft_params
+    params.require(:draft).permit(:title, :url, :comment)
+  end
+end
