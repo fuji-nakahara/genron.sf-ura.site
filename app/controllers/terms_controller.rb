85089494de7d7f62f21f54ceee0fddbd6bf4c217
Fuji Nakahara <fujinakahara2032@gmail.com>
2022-04-11 21:19:11 +0900

    Implement drafts#create

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -3,6 +3,7 @@
 class TermsController < ApplicationController
   def index
     @kadais = Kadai.newest3
+    @drafts = Draft.order(id: :desc)
     @votes = Vote.includes(:user, work: %i[kadai student]).reverse_order.limit(10)
   end
 end
--- spec/requests/drafts_controller_spec.rb
+++ spec/requests/drafts_controller_spec.rb
@@ -1,7 +1,13 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe DraftsController, type: :request do
   describe 'GET /drafts/new' do
+    before do
+      log_in create(:user)
+    end
+
     it 'returns http OK' do
       get new_draft_path
 
