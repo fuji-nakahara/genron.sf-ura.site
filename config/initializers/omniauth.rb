11785c9e4291cc9ccc0a4d0330e4ba9707d8365d
Fuji Nakahara <fujinakahara2032@gmail.com>
2022-04-22 22:26:59 +0900

    Fix rubocop offences of Style/FetchEnvVar

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,7 @@ require 'omniauth/strategies/twitter_dev'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter_dev if Rails.env.development?
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'], {
+  provider :twitter, ENV.fetch('TWITTER_KEY', nil), ENV.fetch('TWITTER_SECRET', nil), {
     secure_image_url: true,
     image_size: 'bigger',
   }
