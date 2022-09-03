b5ba83a3c7ce025e5a3c3307a50c2559901dc452
Fuji Nakahara <fujinakahara2032@gmail.com>
2022-09-04 00:19:51 +0900

    Rename TWITTER_ env vars

--- lib/genron_sf_fun/twitter_client.rb
+++ lib/genron_sf_fun/twitter_client.rb
@@ -5,11 +5,10 @@ module GenronSFFun
     include Singleton
 
     def initialize
-      @consumer_key = ENV.fetch('TWITTER_KEY')
-      @consumer_secret = ENV.fetch('TWITTER_SECRET')
+      @consumer_key = ENV.fetch('TWITTER_CONSUMER_KEY')
+      @consumer_secret = ENV.fetch('TWITTER_CONSUMER_SECRET')
       @access_token = ENV.fetch('TWITTER_ACCESS_TOKEN')
       @access_token_secret = ENV.fetch('TWITTER_ACCESS_TOKEN_SECRET')
-      @bearer_token = ENV.fetch('TWITTER_BEARER_TOKEN')
       super
     end
   end
