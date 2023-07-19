aa64596464b56563f6244dba2e71727a739f60df
Fuji Nakahara <fujinakahara2032@gmail.com>
2023-07-19 14:49:15 +0000

    Add `refresh_token` column to twitter2_credentials

--- db/schemas/twitter2_credentials.rb
+++ db/schemas/twitter2_credentials.rb
@@ -3,6 +3,7 @@
 create_table :twitter2_credentials, force: :cascade do |t|
   t.references :user, null: false, index: { unique: true }, foreign_key: true
 
+  t.string :refresh_token # TODO: Make not null after all refresh_token columns are filled.
   t.string :token, null: false
   t.datetime :expires_at, null: false
 
